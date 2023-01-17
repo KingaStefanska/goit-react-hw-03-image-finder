35b2ba8658aa6b0878d2ba1fae3caa20db5f8d40
KingaStefanska <kinga0stefanska@gmail.com>
2023-01-17 09:04:12 +0100

    poprawki

--- src/API/Api.js
+++ src/API/Api.js
@@ -2,9 +2,9 @@ import axios from 'axios';
 
 axios.defaults.baseURL = 'https://pixabay.com/api/';
 const API_KEY = '29669781-317417cb25e4625a08da5b15a';
-const getImages = async (searchValue, page, perPage) => {
+const getImages = async (searchValue, page) => {
   const response = await axios.get(
-    `?key=${API_KEY}&q=${searchValue}&image_type=photo&orientation=horizontal&safeSearch=true&page=${page}&per_page=${perPage}`
+    `?key=${API_KEY}&q=${searchValue}&image_type=photo&orientation=horizontal&safeSearch=true&page=${page}&per_page=12`
   );
   return response.data;
 };
